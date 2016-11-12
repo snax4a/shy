dd3fc57684b2533f94c935f8c85c39548193b096
nstuyvesant <nates@Nates-iMac.local>
2016-11-12 10:54:10 -0500

    Populate select lists from arrays

--- client/app/cart/cart.component.js
+++ client/app/cart/cart.component.js
@@ -6,6 +6,7 @@ export class CartController {
   /*@ngInject*/
   constructor($window, ProductList, Cart) {
     this.$window = $window;
+    // These have to be set here and not $onInit()
     this.products = ProductList.products;
     this.Cart = Cart;
   }
@@ -18,6 +19,20 @@ export class CartController {
       state: 'PA',
       recipientState: 'PA'
     };
+    this.months = [];
+    for(let i = 1; i < 13; i++) {
+      this.months.push(i);
+    }
+    this.years = [];
+    let currentYear = new Date().getFullYear();
+    for(let i = currentYear; i < currentYear + 10; i++) {
+      this.years.push(i);
+    }
+    // Not needed elsewhere so avoid separate JSON file
+    this.states = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'GU', 'HI',
+      'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME', 'MH', 'MI', 'MN', 'MO', 'MS',
+      'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM', 'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'PR', 'PW',
+      'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VA', 'VI', 'VT', 'WA', 'WI', 'WV', 'WY'];
   }
 
   keepShopping() {
