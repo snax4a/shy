b2f0c2373f820d5e5aa1f1a49551c2a8f6ae84ba
nstuyvesant <nates@Nates-iMac.local>
2016-11-13 21:49:56 -0500

    Fixed window.twttr.widgets loading issue using a $timeout

--- client/app/workshops/workshops.component.js
+++ client/app/workshops/workshops.component.js
@@ -1,18 +1,23 @@
 'use strict';
 import angular from 'angular';
 import routes from './workshops.routes';
-//import uiRouter from 'angular-ui-router';
 
 export class WorkshopsController {
   /*@ngInject*/
-  constructor($http) {
+  constructor($http, $timeout) {
     this.$http = $http;
+    this.$timeout = $timeout;
     this.subscriberEmail = '';
     this.submitted = false;
     this.subscribed = false;
   }
 
   $onInit() {
+    // Wait for Twitter widgets to load
+    this.$timeout(function() {
+      window.twttr.widgets.load();
+    }, 250);
+    // Load the workshops from the JSON file
     this.$http.get('/assets/data/workshops.json')
       .then(response => {
         this.data = response.data;
@@ -23,7 +28,7 @@ export class WorkshopsController {
     this.submitted = true;
     if(form.$valid) {
       // Do an $http.put to add a subscriber to Google docs spreadsheet
-      console.log('Email is valid. Doing HTTP PUT to server.', this.subscriberEmail);
+      this.$log.info('Email is valid. Doing HTTP PUT to server.', this.subscriberEmail);
       this.subscribed = true;
     }
   }
