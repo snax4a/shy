c066176f9ec03792512369592769838d2d3b69dc
nstuyvesant <nates@Nates-iMac.local>
2016-11-19 12:12:35 -0500

    Add subscribe endpoint

--- server/api/subscribe/index.js
+++ server/api/subscribe/index.js
@@ -2,9 +2,7 @@
 
 var express = require('express');
 var controller = require('./subscribe.controller');
-
 var router = express.Router();
-
 router.get('/', controller.index);
 
 module.exports = router;
