64458eefebdc460f7b2a3edc91f67bd1dfb0531b
Nate Stuyvesant <nstuyvesant@gmail.com>
2016-12-10 09:10:47 -0500

    Update sample file

--- server/config/local.env.sample.js
+++ server/config/local.env.sample.js
@@ -14,15 +14,7 @@ module.exports = {
 
   DATABASE_URL: 'postgres://user:pass@localhost:5432/dbname',
 
-  BRAINTREE_ENVIRONMENT: 'braintree.Environment.Sandbox', // not to be in quotes - use require
   BRAINTREE_MERCHANT_ID: 'replace with braintree merchant id',
   BRAINTREE_PUBLIC_KEY: 'replace with braintree public key',
-  BRAINTREE_PRIVATE_KEY: 'replace with braintree private key',
-
-  PAYFLOW_PRO_URL: 'https://payflowpro.paypal.com',
-  PAYFLOW_PRO_USER: 'User',
-  PAYFLOW_PRO_PASSWORD: 'Password',
-  PAYFLOW_PRO_PARTNER: 'PayPal',
-  PAYFLOW_PRO_VENDOR: 'Vendor',
-  PAYFLOW_PRO_TESTING: true
+  BRAINTREE_PRIVATE_KEY: 'replace with braintree private key'
 };
