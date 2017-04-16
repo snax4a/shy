bf6abd1861ece9afa2791e7a53a22687cbf15bc3
Nate Stuyvesant <nstuyvesant@gmail.com>
2017-04-16 15:23:17 -0400

    refactor(webpack): optimize

--- webpack.make.js
+++ webpack.make.js
@@ -5,38 +5,24 @@ import autoprefixer from 'autoprefixer';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import HtmlWebpackHarddiskPlugin from 'html-webpack-harddisk-plugin';
-//import CompressionPlugin from 'compression-webpack-plugin';
 import path from 'path';
 import webpack from 'webpack';
+//import CompressionPlugin from 'compression-webpack-plugin';
 
 module.exports = function makeWebpackConfig(options) {
   // Set by gulpfile.babel.js or karma.conf.js
   const BUILD = !!options.BUILD;
-  const TEST = !!options.TEST;
-  const E2E = !!options.E2E;
+  const TEST = options.TEST || options.E2E;
   const DEV = !!options.DEV;
 
   // Establish the base configuration
   let config = {
     cache: DEV,
 
-    // context: __dirname, // current directory by default
-
-    // Not relevant as webpack-dev-server not in use
-    // devServer: {
-    //   contentBase: './client/',
-    //   stats: {
-    //     modules: false,
-    //     cached: false,
-    //     chunks: false
-    //   }
-    // },
-
     devtool: '', // placeholder to be filled in conditionally
 
     entry: TEST ? '' : { // If test, set entry to '' to avoid Karma error (bug)
-      app: [/*'babel-polyfill', */'./client/app/app.js'], // should 'babel-polyfill' go here in front of app.js?
-      polyfills: 'babel-polyfill', // must use polyfill for ie support instead of transform-runtime
+      polyfills: 'babel-polyfill', // Needed for IE - transform-runtime doesn't work
       vendor: [ // bundle stuff that changes seldomly
         'angular',
         'angular-aria',
@@ -49,7 +35,8 @@ module.exports = function makeWebpackConfig(options) {
         'angular-ui-router',
         'angular-utils-pagination',
         'braintree-web'
-      ]
+      ],
+      app: [/*'babel-polyfill', */'./client/app/app.js'] // should 'babel-polyfill' go here in front of app.js?
     },
 
     module: {
@@ -70,8 +57,9 @@ module.exports = function makeWebpackConfig(options) {
             //   }]
             // ],
             // plugins: ['transform-class-properties'],
-            shouldPrintComment: commentContents => /@ngInject/.test(commentContents), // leave ng-annotate alone
-            cacheDirectory: true
+            cacheDirectory: true,
+            minified: true,
+            shouldPrintComment: commentContents => /@ngInject/.test(commentContents) // leave ng-annotate alone
           },
           include: [
             path.resolve(__dirname, 'client/')
@@ -85,7 +73,7 @@ module.exports = function makeWebpackConfig(options) {
 
         {
           test: /\.pug$/,
-          //loader: 'pug-loader' // https://github.com/pugjs/pug-loader
+          // loader: 'pug-loader' // https://github.com/pugjs/pug-loader // adds 6K
           use: ['raw-loader', 'pug-html-loader'] // https://github.com/willyelm/pug-html-loader
         },
 
@@ -97,25 +85,21 @@ module.exports = function makeWebpackConfig(options) {
               {
                 loader: 'css-loader', // https://github.com/webpack-contrib/css-loader
                 options: {
-                  sourceMap: false, // rarely need to debug CSS
                   minimize: true,
-                  discardComments: {removeAll: true}
+                  sourceMap: false
                 }
               },
-              {
+              { // Shrinks CSS file by 12K
                 loader: 'postcss-loader', // https://github.com/postcss/postcss-loader
                 options: {
-                  sourceMap: false,
-                  plugins: () => [autoprefixer({browsers: ['last 2 versions']})]
+                  plugins: () => [autoprefixer({browsers: ['last 2 versions']})],
+                  sourceMap: false
                 }
               },
               {
                 loader: 'sass-loader', // https://github.com/webpack-contrib/sass-loader
                 options: {
-                  sourceMap: false, // rarely need to debug CSS
                   outputStyle: 'compressed',
-                  precision: 10,
-                  sourceComments: false
                 }
               }
             ]
@@ -126,8 +110,6 @@ module.exports = function makeWebpackConfig(options) {
         },
 
         {
-          // Explore https://github.com/jeffling/ng-annotate-webpack-plugin as
-          // possible replacement for ng-annotate-loader (seemed slow though)
           test: /\.js$/,
           loader: 'ng-annotate-loader?single_quotes', // https://github.com/huston007/ng-annotate-loader
           enforce: 'post'
@@ -161,11 +143,6 @@ module.exports = function makeWebpackConfig(options) {
       //   threshold: 10240,
       //   minRatio: 0.8
       // })
-
-      // UglifyJSPlugin no longer switches loaders into minimize mode
-      // new webpack.LoaderOptionsPlugin({
-      //   minimize: true
-      // })
     ]
   };
 
@@ -183,9 +160,8 @@ module.exports = function makeWebpackConfig(options) {
       // Absolute output directory
       path: BUILD ? path.join(__dirname, '/dist/client/') : path.join(__dirname, '/.tmp/'),
 
-      // Output path from the view of the page - uses webpack-dev-server in development
-      publicPath: BUILD || DEV || E2E ? '/' : `http://localhost:${8080}/`,
-      //publicPath: BUILD ? '/' : `http://localhost:${env.port}/`,
+      // Output path from the view of the page
+      publicPath: '/',
 
       // Filename for entry points - add hash in build mode
       filename: BUILD ? '[name].[hash].js' : '[name].bundle.js',
@@ -199,7 +175,7 @@ module.exports = function makeWebpackConfig(options) {
       new webpack.optimize.CommonsChunkPlugin({
         name: 'vendor', // vendor.js
         minChunks: Infinity // chunk is only for vendor JS
-        //minChunks: (module, count) =>  module.resource && module.resource.indexOf(path.resolve('node_modules')) === 0
+        //minChunks: (module, count) => module.context && module.context.indexOf("node_modules") >= 0;
       }),
 
       // Don't render index.html
