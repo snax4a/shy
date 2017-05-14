9741d1ebc886de29a82165cb14803d4c94b2c7ce
Nate Stuyvesant <nstuyvesant@gmail.com>
2017-05-14 23:51:24 +0300

    fix(auth): admin as superset of teacher permissions

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -4,14 +4,14 @@ const controller = require('./user.controller');
 const router = express.Router();
 const auth = require('../../auth/auth.service');
 
-router.get('/', auth.hasRole('admin') || auth.hasRole('teacher'), controller.index); // admin, get users
+router.get('/', auth.hasRole('teacher'), controller.index); // teacher, admin, get users
 router.get('/me', auth.isAuthenticated(), controller.me); // user, retrieve profile
 
 router.post('/', controller.create); // sign-up and login
 router.post('/forgotpassword', controller.forgotPassword); // gen new password and email
 
 router.put('/:id', auth.isAuthenticated(), controller.update); // user, update profile
-router.put('/:id/admin', auth.hasRole('admin') || auth.hasRole('teacher'), controller.upsert); // admin, update existing user
+router.put('/:id/admin', auth.hasRole('teacher'), controller.upsert); // teacher, admin, update existing user
 
 router.delete('/:id', auth.hasRole('admin'), controller.destroy); // admin, delete user
 
--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports = module.exports = {
-  // List of user roles
+  // List of user roles - order is important for auth.hasRole() as it looks for roles that are >=
   userRoles: ['student', 'teacher', 'admin'],
 
   // List of providers (less local)
