c52017a5fd88229d3e4eb018af7b38d0d300a80c
Nate Stuyvesant <nstuyvesant@gmail.com>
2017-06-01 19:24:14 -0400

    fix(test): user model spec

--- server/api/user/user.model.spec.js
+++ server/api/user/user.model.spec.js
@@ -17,18 +17,11 @@ let buildUser = function() {
 };
 
 describe('User Model', function() {
-  // before(function() {
-  //   // Sync and clear users before testing
-  //   return User.sync().then(function() {
-  //     return User.destroy({ where: { email: 'test@example.com' } });
-  //   });
-  // });
 
   beforeEach(function() {
     return User.sync().then(function() {
       return User.destroy({ where: { email: 'test@example.com' } }).then(buildUser);
     });
-    //buildUser();
   });
 
   afterEach(function() {
