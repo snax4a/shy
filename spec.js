adbf2beb9696a8c3b9c6458f082a519e267ecaa9
Nate Stuyvesant <nstuyvesant@gmail.com>
2017-06-01 17:56:54 -0400

    fix(test:client): removed ref to angular-ui-router

--- spec.js
+++ spec.js
@@ -4,7 +4,6 @@ var testsContext;
 
 require('angular');
 require('angular-mocks');
-require('./client/components/ui-router/ui-router.mock');
 
 testsContext = require.context('./client', true, /\.spec\.js$/);
 testsContext.keys().forEach(testsContext);
