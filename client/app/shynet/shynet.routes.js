d327b21f273a9aeda2fc399e84d54af5d0b0665b
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-11-12 21:08:37 -0500

    fix(shynet): prompt for authentication before accessing page

--- client/app/shynet/shynet.routes.js
+++ client/app/shynet/shynet.routes.js
@@ -3,7 +3,9 @@
 export default function($routeProvider) {
   'ngInject';
   $routeProvider.when('/shynet', {
+    url: '/shynet',
     template: '<shynet></shynet>',
+    authenticate: 'teacher',
     title: 'SHYnet'
   });
 }
