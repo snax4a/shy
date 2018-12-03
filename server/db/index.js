52e06ec968e7652c1d45f817eb60a48585c6a6d0
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-12-03 17:30:48 -0500

    docs(comments): update

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,12 @@
 // Centralize use of pg throughout project
 import { Pool } from 'pg';
+import config from '../../config/environment';
 
-const pool = new Pool();
+const pool = new Pool({ connectionString: config.pg.uri });
+// Read https://node-postgres.com/features/pooling
 
+// TODO: Call pool.end() within the pool.query callback or promise.
 export default {
   query: (text, params) => pool.query(text, params)
+  // .then
 };
