11baf9a654d9350f4691f500dfca7f393bd21518
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-12-09 13:06:22 -0500

    chore(express-promise-router): build experiment

--- server/api/announcement/announcement2.controller.js
+++ server/api/announcement/announcement2.controller.js
@@ -1,45 +1,7 @@
 'use strict';
+const db = require('../../db');
 
-import * as db from '../../db';
-
-// Passes JSON back so that UI fields can be flagged for validation issues
-// function validationError(res, statusCode) {
-//   statusCode = statusCode || 422;
-//   return err => res.status(statusCode).json(err);
-// }
-
-// function handleEntityNotFound(res) {
-//   return function(entity) {
-//     if(!entity) {
-//       res.status(404).end();
-//       return null;
-//     }
-//     return entity;
-//   };
-// }
-
-// function removeEntity(res) {
-//   return function(entity) {
-//     if(entity) {
-//       return entity.destroy()
-//         .then(() => res.status(204).end());
-//     }
-//     throw new Error('Entity to remove was not provided'); // handleEntityNotFound should have prevented this
-//   };
-// }
-
-// function handleError(res, statusCode) {
-//   statusCode = statusCode || 500;
-//   return err => res.status(statusCode).send(err);
-// }
-
-// Gets a list of Announcements
-export async function index(req, res) {
-  const flat = req.query.flat;
-  const { announcements } = await db.query('SELECT _id, section, title, description, expires FROM "Announcements" WHERE expires > CURRENT_DATE ORDER BY section, expires;');
-  return flat ? res.status(200).json(announcements) : res.status(200).json(nest(announcements));
-}
-
+// Utility function to return nested JSON from flat list
 function nest(flatAnnouncements) {
   let nestedAnnouncements = [];
   let currentSection;
@@ -71,39 +33,33 @@ function nest(flatAnnouncements) {
   return nestedAnnouncements;
 }
 
+// Returns list of Announcements
+export async function index(req, res) {
+  const { rows } = await db.query('SELECT _id, section, title, description, expires FROM "Announcements" WHERE expires > CURRENT_DATE ORDER BY section, expires;');
+  return req.query.flat ? res.status(200).json(rows) : res.status(200).json(nest(rows));
+}
+
 // Updates or creates announcement (admin-only)
 export async function upsert(req, res) {
   const { _id, section, title, description, expires } = req.body;
   const isNew = _id === 0; // zero means INSERT
-  let sql = '';
+  let sql;
 
   if(isNew) {
     sql = 'INSERT INTO "Announcements" (section, title, description, expires) VALUES ($2, $3, $4, $5);';
   } else {
-    sql = 'UPDATE "Announcements" SET section = $2, title = $3, description = $4, expires = $5 WHERE _id = $1';
+    sql = 'UPDATE "Announcements" SET section = $2, title = $3, description = $4, expires = $5 WHERE _id = $1;';
   }
-  const { results } = await db.query(sql, [_id, section, title, description, expires]);
-  // TODO: Get new ID value if there was an INSERT based on https://github.com/brianc/node-postgres/wiki/FAQ#7-i-just-have-a-question-and-maybe-a-feature-request-that-i-am-not-able-to-think-about-how-to-implement-or-do-it-i-need-to-retrieve-the-inserted-row-or-someway-to-reach-it-after-the-insert-is-done
-
-  if(isNew) Reflect.deleteProperty(req.body, '_id');
-  let announcementToUpsert = Announcement.build(req.body);
-  announcementToUpsert.isNewRecord = isNew;
-
-  return announcementToUpsert.save()
-    .then(announcement => res.status(200).json({ _id: announcement._id }))
-    .catch(validationError(res));
+  const { rows } = await db.query(sql, [_id, section, title, description, expires]);
+  res.status(200).json({ _id: rows[0].id });
 }
 
 // Deletes announcement (admin-only)
-export function destroy(req, res) {
-  return Announcement.find({
-    where: {
-      _id: req.params.id
-    }
-  })
-    .then(handleEntityNotFound(res))
-    .then(removeEntity(res))
-    .catch(handleError(res));
+export async function destroy(req, res) {
+  const _id = req.params.id;
+  const sql = 'DELETE FROM "Announcements" WHERE _id = $1;';
+  await db.query(sql, [_id]);
+  res.status(200).json({ message: `Announcement ${_id} deleted.`});
 }
 
 // Authentication callback
--- server/api/announcement/index2.js
+++ server/api/announcement/index2.js
@@ -1,12 +1,11 @@
 'use strict';
-import { Router } from 'express-promise-router';
-import * as controller from './announcement.controller';
-import * as auth from '../../auth/auth.service';
+const Router = require('express-promise-router');
+const controller = require('./announcement2.controller');
+const auth = require('../../auth/auth.service');
 const router = new Router();
 
-// async router example w/pg: https://node-postgres.com/guides/async-express
 router.get('/', controller.index);
 router.put('/:id', auth.hasRole('admin'), controller.upsert); // admin, update existing announcement
 router.delete('/:id', auth.hasRole('admin'), controller.destroy); // admin, delete announcement
 
-export default router;
+module.exports = router;
