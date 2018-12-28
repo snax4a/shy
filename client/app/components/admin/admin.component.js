412f656b302c7bd3dbf1d88778c73d692fe88234
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-12-28 16:39:00 -0500

    fix(schedulemanager): admin ref to

--- client/app/components/admin/admin.component.js
+++ client/app/components/admin/admin.component.js
@@ -5,14 +5,14 @@ import ngRoute from 'angular-route';
 import routes from './admin.routes';
 import UibTabsDirective from 'angular-ui-bootstrap/src/tabs';
 import AnnouncementManagerModule from '../../modules/announcementmanager/announcementmanager.module';
-import ScheduleManagerComponent from '../schedulemanager/schedulemanager.component';
+import ScheduleManagerModule from '../../modules/schedulemanager/schedulemanager.module';
 import UserManagerModule from '../../modules/usermanager/usermanager.module';
 
 export class AdminComponent {
   /*@ngInject*/
 }
 
-export default angular.module('shyApp.admin', [ngRoute, AnnouncementManagerModule, UibTabsDirective, ScheduleManagerComponent, UserManagerModule])
+export default angular.module('shyApp.admin', [ngRoute, AnnouncementManagerModule, UibTabsDirective, ScheduleManagerModule, UserManagerModule])
   .config(routes)
   .component('admin', {
     template: require('./admin.pug'),
