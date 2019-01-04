45d5f45b5c1c0381321d43ea8e56916d01236036
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-01-03 22:50:40 -0500

    refactor(mail): sending using node https

--- server/mail/index.js
+++ server/mail/index.js
@@ -1,8 +1,8 @@
+import https from 'https';
 import config from '../config/environment';
-import SibApiV3Sdk from 'sib-api-v3-sdk';
 
 /*
-  Sample message input:
+  Sample message input format:
   {
     sender: [{ email: 'foo@example.com', name: 'Something' }], //sender: config.mail.sender,
     to: [{ email: 'foo@example.com', name: 'Something' }],
@@ -11,22 +11,40 @@ import SibApiV3Sdk from 'sib-api-v3-sdk';
     tags: ['myTag1', 'myTag2']
   }
 */
+
+const sendTransactionalEmail = message => new Promise((resolve, reject) => {
+  const data = JSON.stringify(message);
+  const options = {
+    hostname: 'api.sendinblue.com',
+    path: '/v3/smtp/email',
+    method: 'POST',
+    headers: {
+      'api-key': config.mail.apiKey,
+      'Content-Type': 'application/json',
+      'Content-Length': data.length
+    }
+  };
+  const req = https.request(options, res => {
+    res.on('data', buffer => {
+      if(res.statusCode === 201) return resolve(buffer.toString('utf8'));
+      reject('Unexpected response from SendInBlue');
+    });
+    req.on('error', err => {
+      console.error('Error communicating with SendInBlue', err);
+      reject('Error communicating with SendInBlue');
+    });
+  });
+  req.write(data);
+  req.end();
+});
+
 export default {
   send: async message => {
-    // SendInBlue authentication
-    let defaultClient = SibApiV3Sdk.ApiClient.instance;
-    let authentications = defaultClient.authentications['api-key'];
-    authentications.apiKey = config.mail.apiKey;
-
-    // Compose email (no constructor available in v3)
-    let messageToSend = new SibApiV3Sdk.SendSmtpEmail();
-    messageToSend = { ...message };
     try {
-      let apiInstance = new SibApiV3Sdk.SMTPApi();
-      const data = await apiInstance.sendTransacEmail(messageToSend);
-      return await data;
+      return await sendTransactionalEmail(message);
     } catch(err) {
       console.error(err);
+      return 'Did not send the email.';
     }
   }
 };
