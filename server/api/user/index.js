05cab5efc42c8d5110a05e48ab14427dd37dd5b0
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-01-15 15:32:58 -0500

    docs(comments): update

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -5,8 +5,8 @@ import asyncWrapper from '../../middleware/async-wrapper'; // only wrap async fu
 
 const router = Router();
 
-router.get('/', auth.hasRole('teacher'), asyncWrapper(controller.index)); // teacher, admin, get users
-router.get('/me', auth.isAuthenticated(), asyncWrapper(controller.me)); // user, retrieve profile
+router.get('/', auth.hasRole('teacher'), asyncWrapper(controller.index)); // teacher/admin - get users
+router.get('/me', auth.isAuthenticated(), asyncWrapper(controller.me)); // any authenticated user - retrieve profile
 router.get('/unsubscribe/:email', asyncWrapper(controller.unsubscribe));
 
 router.post('/', asyncWrapper(controller.create)); // sign-up and login
@@ -14,9 +14,9 @@ router.post('/subscribe', asyncWrapper(controller.subscribe));
 router.post('/message', asyncWrapper(controller.messageSend));
 router.post('/forgotpassword', asyncWrapper(controller.forgotPassword)); // gen new password and email
 
-router.put('/:id', auth.isAuthenticated(), asyncWrapper(controller.update)); // user - update profile
+router.put('/:id', auth.isAuthenticated(), asyncWrapper(controller.update)); // authenticated user - update profile
 router.put('/:id/admin', auth.hasRole('teacher'), asyncWrapper(controller.upsert)); // teacher/admin - update existing user
 
-router.delete('/:id', auth.hasRole('admin'), asyncWrapper(controller.destroy)); // admin, delete user
+router.delete('/:id', auth.hasRole('admin'), asyncWrapper(controller.destroy)); // admin - delete user
 
 export default router;
