bd29d5517fb3d315ca0f54f8ddec9f6c167f6d21
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-02-01 12:25:09 -0500

    chore(usermanager): add ids

--- cypress/integration/login-logout.e2e.test.js
+++ cypress/integration/login-logout.e2e.test.js
@@ -34,10 +34,12 @@ describe('Login/Logout as Student, Teacher, Admin', () => {
 
   it('should login a teacher with a local account', () => {
     loginLocal(Cypress.env('TEACHER_EMAIL'), Cypress.env('TEACHER_PASSWORD'), '/shynet');
+    logout();
   });
 
   it('should login an admin with a local account', () => {
     loginLocal(Cypress.env('ADMIN_EMAIL'), Cypress.env('ADMIN_PASSWORD'), '/admin');
+    logout();
   });
 
   it('should error if a Google user tries to reset their password', () => {
@@ -49,12 +51,19 @@ describe('Login/Logout as Student, Teacher, Admin', () => {
     cy.get('#cancel').click();
   });
 
-  // TODO: Must administratively reset this password afterwards to keep in known state
-  // it('should allow a user to reset their password', () => {
-  //   cy.visit('/login');
-  //   cy.get('#forgotPassword').click();
-  //   cy.get('#email').type(Cypress.env('STUDENT_EMAIL'));
-  //   cy.get('#sendPassword').click();
-  //   cy.contains('A new password was emailed to you. Please check your Junk folder if you don\'t see it.');
-  // });
+  it('should allow a user to reset their password', () => {
+    cy.visit('/login');
+    cy.get('#forgotPassword').click();
+    cy.get('#email').type(Cypress.env('STUDENT_EMAIL'));
+    cy.get('#sendPassword').click();
+    cy.contains('A new password was emailed to you. Please check your Junk folder if you don\'t see it.');
+    loginLocal(Cypress.env('ADMIN_EMAIL'), Cypress.env('ADMIN_PASSWORD'), '/admin');
+    cy.get('#filterField').type(Cypress.env('STUDENT_EMAIL'));
+    cy.get('#search').click();
+    cy.get('#edit').click();
+    cy.get('#passwordNew').type(Cypress.env('STUDENT_PASSWORD'));
+    cy.get('#passwordConfirm').type(Cypress.env('STUDENT_PASSWORD'));
+    cy.get('#save').click();
+    logout();
+  });
 });
