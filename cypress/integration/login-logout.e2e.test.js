43ece7a9bd170edf0a5e6d59582755f732c31ccd
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-02-01 12:11:40 -0500

    chore(e2e): build login-logout and enhance app to make more testable

--- cypress/integration/login-logout.e2e.test.js
+++ cypress/integration/login-logout.e2e.test.js
@@ -1,55 +1,60 @@
 /// <reference types="Cypress" />
 
 /* globals describe, it, cy, Cypress, expect, beforeEach  */
-// import dotenv from 'dotenv';
-// dotenv.config();
-// console.log(process.env);
-//import config from '../../server/config/environment';
 
 const loginLocal = (username, password, expectedUrl) => {
-  cy.visit('http://localhost:3000/login');
+  cy.visit('/login');
   cy.get('#email').type(username);
   cy.get('#password').type(password);
   cy.get('#login').click();
   cy.url().should('include', expectedUrl);
 };
 
+const logout = () => {
+  cy.get('#username').click();
+  cy.get('#logout').click();
+};
+
 describe('Login/Logout as Student, Teacher, Admin', () => {
+  // Not working due to CORS issue with Google redirect
   // it('should login an admin via Google+', () => {
-  //   cy.visit('http://localhost:3000/login');
+  //   cy.visit('/login');
   //   cy.get('#google').click();
   // });
 
   it('should reject a user with incorrect credentials', () => {
-    console.log(Cypress.env('NODE_ENV'));
     loginLocal('bogususer@bitbucket.com', 'knownbadpassword', '/login');
     cy.contains('Unrecognized username/password combination.');
   });
 
-  // it('should login a student with a local account', () => {
-  //   loginLocal(config.student.email, config.student.password, '');
-  //   cy.get('#logout').click();
-  // });
+  it('should login a student with a local account', () => {
+    loginLocal(Cypress.env('STUDENT_EMAIL'), Cypress.env('STUDENT_PASSWORD'), '/');
+    logout();
+  });
 
-  // it('should login a teacher with a local account', () => {
-  //   loginLocal(config.teacher.email, config.teacher.password, '/shynet');
-  // });
+  it('should login a teacher with a local account', () => {
+    loginLocal(Cypress.env('TEACHER_EMAIL'), Cypress.env('TEACHER_PASSWORD'), '/shynet');
+  });
 
-  // it('should login an admin with a local account', () => {
-  //   loginLocal(config.admin.email, config.admin.password, '/admin');
-  // });
+  it('should login an admin with a local account', () => {
+    loginLocal(Cypress.env('ADMIN_EMAIL'), Cypress.env('ADMIN_PASSWORD'), '/admin');
+  });
 
-  // it('should error if a Google user tries to reset their password', () => {
-  //   cy.visit('http://localhost:3000/login');
-  //   cy.contains('Forgot password?').click();
-  //   cy.get('.ng-valid-email').type(config.google.email);
-  //   cy.contains('Send Password').click();
-  //   cy.contains('Please visit https://myaccount.google.com/security if you forgot your password.');
-  // });
+  it('should error if a Google user tries to reset their password', () => {
+    cy.visit('/login');
+    cy.get('#forgotPassword').click();
+    cy.get('#email').type(Cypress.env('GOOGLE_EMAIL'));
+    cy.get('#sendPassword').click();
+    cy.contains('Please visit https://myaccount.google.com/security if you forgot your password.');
+    cy.get('#cancel').click();
+  });
 
+  // TODO: Must administratively reset this password afterwards to keep in known state
   // it('should allow a user to reset their password', () => {
-  //   cy.visit('http://localhost:3000/login');
-  //   cy.contains('Forgot password?').click();
-  //   cy.contains('.ng-valid-email').type(config.student.email);
+  //   cy.visit('/login');
+  //   cy.get('#forgotPassword').click();
+  //   cy.get('#email').type(Cypress.env('STUDENT_EMAIL'));
+  //   cy.get('#sendPassword').click();
+  //   cy.contains('A new password was emailed to you. Please check your Junk folder if you don\'t see it.');
   // });
 });
