5ecc20b66921732f4cb5da78c044fb8e800bb747
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-02-04 23:39:53 -0500

    chore(e2e): classes

--- cypress/integration/admin/classes.e2e.test.js
+++ cypress/integration/admin/classes.e2e.test.js
@@ -3,8 +3,9 @@
 /* globals describe, it, cy, Cypress  */
 
 describe('Admin -> Classes Feature', () => {
-  it('should login admin and navigate to Announcements tab', () => {
-    cy.login(Cypress.env('ADMIN_EMAIL'), Cypress.env('ADMIN_PASSWORD'), '/admin');
+  beforeEach(() => {
+    cy.loginApi(Cypress.env('ADMIN_EMAIL'), Cypress.env('ADMIN_PASSWORD'));
+    cy.visit('/admin');
     cy.get('#schedule').click();
   });
 
@@ -12,23 +13,28 @@ describe('Admin -> Classes Feature', () => {
     cy.contains('New Schedule Item').click();
     cy.get('#location').select('Squirrel Hill');
     cy.get('#day').select('Sunday');
-    cy.get('#startTime').type('18:00');
-    cy.get('#endTime').type('19:30');
+    cy.get('#startTime').clear()
+      .type('18:00');
+    cy.get('#endTime').clear()
+      .type('19:30');
     cy.get('#title').select('Yoga 1');
     cy.get('#teacher').select('Koontz, Leta');
     cy.get('#canceled').select('No');
     cy.get('#save').click();
   });
 
-  // it('should save changes to an existing schedule item', () => {
-  //   cy.contains('Section Test').click();
-  //   cy.get('#section').type('Section Test Changed');
-  //   cy.get('#title').type('Title Test Changed');
-  //   cy.get('#description').type('Description Test Changed');
-  //   cy.get('#save').click();
-  // });
+  it('should save changes to an existing schedule item', () => {
+    cy.contains('tr', 'Leta Koontz').get('td a')
+      .first()
+      .click();
+    cy.get('#location').select('East Liberty');
+    cy.get('#canceled').select('Yes');
+    cy.get('#save').click();
+  });
 
-  // it('should delete a schedule item', () => {
-  //   //cy.logout();
-  // });
+  it('should delete a schedule item', () => {
+    cy.contains('tr', 'Leta Koontz')
+      .find('.trash')
+      .click();
+  });
 });
