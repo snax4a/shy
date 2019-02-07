d3ad20d4e88bd333d3bde705ded28c0d006998e7
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-02-07 11:21:30 -0500

    chore(e2e): improve users

--- cypress/integration/admin/users.e2e.test.js
+++ cypress/integration/admin/users.e2e.test.js
@@ -2,10 +2,39 @@
 
 /* globals describe, it, cy, Cypress  */
 
+const find = email => {
+  cy.get('#filterField')
+    .clear()
+    .type(email);
+  cy.get('#search').click();
+  cy.contains(email);
+};
+
 describe('Admin -> Users Feature', () => {
   beforeEach(() => {
     cy.loginApi(Cypress.env('ADMIN_EMAIL'), Cypress.env('ADMIN_PASSWORD'));
     cy.visit('/admin');
+  });
+
+
+  it('should find existing user', () => {
+    find(Cypress.env('ADMIN_EMAIL'));
+  });
+
+  it('should get no results when searching for user who does not exist', () => {
+    cy.get('#filterField')
+      .clear()
+      .type('no_one_who_exists');
+    cy.get('#search').click();
+    cy.contains('tr', 'No users found meeting the search criteria.');
+  });
+
+  it('should error when creating new user if required fields are empty or invalid', () => {
+    cy.contains('New User').click();
+    cy.get('#save').click();
+    cy.contains('Please enter the first name.');
+    cy.contains('Please enter the last name.');
+    cy.contains('Please enter the email address.');
   });
 
   it('should create a new student', () => {
@@ -26,10 +55,7 @@ describe('Admin -> Users Feature', () => {
   });
 
   it('should save changes to an existing student', () => {
-    cy.get('#filterField')
-      .clear()
-      .type('testy.mctestface@bitbucket.com');
-    cy.get('#search').click();
+    find('testy.mctestface@bitbucket.com');
     cy.contains('Mctestface, Testy').click();
     cy.get('#email').clear()
       .type('testy.mctestface@bitbucket.com');
@@ -38,10 +64,7 @@ describe('Admin -> Users Feature', () => {
   });
 
   it('should delete a user', () => {
-    cy.get('#filterField')
-      .clear()
-      .type('testy.mctestface@bitbucket.com');
-    cy.get('#search').click();
+    find('testy.mctestface@bitbucket.com');
     cy.contains('tr', 'Mctestface, Testy')
       .find('.trash')
       .click();
