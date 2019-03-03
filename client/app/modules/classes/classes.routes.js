1cb0fa9f5917f3eb6c33ec00d36627395b88909d
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-03-03 11:20:52 -0500

    fix(classes): page title

--- client/app/modules/classes/classes.routes.js
+++ client/app/modules/classes/classes.routes.js
@@ -2,7 +2,7 @@ export default function($routeProvider) {
   'ngInject';
   $routeProvider.when('/classes', {
     template: '<classes></classes>',
-    title: 'Schoolhouse Yoga Pittsburgh - Classes at our 3 schools',
+    title: 'Schoolhouse Yoga Pittsburgh - Class Schedules for our 3 schools',
     resolve: {
       '': (LocationService, ClassService) => Promise.all([ClassService.initialized, LocationService.initialized])
     }
