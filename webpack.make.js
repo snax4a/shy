8f43461dd7c04335ded8ba21d48be20d36b0e19c
Nate Stuyvesant <nstuyvesant@gmail.com>
2019-04-23 10:43:03 -0400

    fix(webpack): comment unused items

--- webpack.make.js
+++ webpack.make.js
@@ -8,8 +8,8 @@ import path from 'path';
 import webpack from 'webpack';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import MomentLocalesPlugin from 'moment-locales-webpack-plugin';
-import PurgecssPlugin from 'purgecss-webpack-plugin';
-import glob from 'glob-all';
+// import PurgecssPlugin from 'purgecss-webpack-plugin';
+// import glob from 'glob-all';
 
 // Called by gulpfile.babel.js, and server/config/express.js
 export default function makeWebpackConfig(mode) {
